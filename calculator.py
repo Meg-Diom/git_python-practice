2352b6c3a2550a24e3700462304e1cca69b2c8ac
Meg-Diom <d.ngum1@alustudent.com>
2026-07-19 14:52:15 +0400

    Modified Python Calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-import math
+import cmath
 
 a = float(input("Enter the coeffitient of x squared: "))
 b = float(input("Enter the coeffitient of x:" ))
@@ -6,7 +6,7 @@ c = float(input("Enter the coeffitient of the temm without x: "))
 
 det = (b**2) - (4*a*c)
 
-x_1 = (-b + math.sqrt(det))/(2*a)
-x_2 = (-b - math.sqrt(det))/(2*a)
+x_1 = (-b + cmath.sqrt(det))/(2*a)
+x_2 = (-b - cmath.sqrt(det))/(2*a)
 
 print(f"The solution to the quadratic equation are:\nx_1: {x_1}.\nx_2: {x_2}")
